ef50c673cc4283048ba5fa76c14736b2090ba384
Aditya <workwith.adityapatil@gmail.com>
2025-07-24 01:53:40 +0530

    Solved 2 questions

--- Chapter1.js
+++ Chapter1.js
@@ -1,10 +1,11 @@
-// Create a const object called "Product" to store info 
-const Product = {
-    name : "Parker Jotter Standard CT Ball Pen",
-    colour : "black",
-    rating:4,
-    isDeal:true,
-    price : 270,
-    offer:"5%"
-}
-console.log(Product);
+// Qs2. Create a const object called “profile” to store information shown in the picture.
+const Product2 = {
+    name:"TonyStark",
+    isFollow:true,
+    followers:1000,
+    following:100,
+    description:"To Code Better"
+};
+
+console.log(Product2);
+
