8f0b742e053f3de3c73d0697b8b46f64b59cb418
Aditya <workwith.adityapatil@gmail.com>
2025-07-24 01:48:12 +0530

    Solved 1 question

--- Chapter1.js
+++ Chapter1.js
@@ -1,1 +1,10 @@
-console.log("Hello World ");
+// Create a const object called "Product" to store info 
+const Product = {
+    name : "Parker Jotter Standard CT Ball Pen",
+    colour : "black",
+    rating:4,
+    isDeal:true,
+    price : 270,
+    offer:"5%"
+}
+console.log(Product);
