df02d85cece338122e049f3486b672cfdc934595
Aditya <workwith.adityapatil@gmail.com>
2025-07-24 01:58:05 +0530

    Solved all questions

--- Chapter1.js
+++ Chapter1.js
@@ -1,3 +1,14 @@
+// Qs1. Create a const object called “product” to store information shown in the picture.
+const Product1 ={
+    name:"Parker Jotter Standard Ball Pen",
+    rating:4,
+    price:270,
+    isDeal:true,
+    offer:"5%"
+};
+
+console.log(Product1);
+
 // Qs2. Create a const object called “profile” to store information shown in the picture.
 const Product2 = {
     name:"TonyStark",
